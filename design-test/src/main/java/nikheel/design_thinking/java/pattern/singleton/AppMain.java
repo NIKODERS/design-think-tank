441d2da7a57a933a328e73e6aa32370611b7f226
nikhe <nikhe@host.docker.internal>
2024-07-10 06:13:21 +0530

    first commit

---
+++ design-test/src/main/java/nikheel/design_thinking/java/pattern/singleton/AppMain.java
@@ -0,0 +1,9 @@
+package nikheel.design_thinking.java.pattern.singleton;
+
+public class AppMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+
+}
